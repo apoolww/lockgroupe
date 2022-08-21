04675189188e87fefe15ae595cbe7bcb1ccaa98b
apoo <103772518+apoolww@users.noreply.github.com>
2022-08-21 20:19:50 +0200

    Add files via upload

---
+++ lock.js
@@ -0,0 +1,29 @@
+const { Client } = require('discord.js-selfbot-v13'),
+    request = require('request'),
+    client = new Client(),
+    user = 'votre_id'
+
+client.on('messageCreate', async message => {
+    if (message.content === '!lock') {
+        message.edit('...')
+        setInterval(async () => {
+            request(`https://discord.com/api/v9/channels/${message.channel.id}/recipients/${user}`, {
+                "headers": {
+                    "accept": "*/*",
+                    "authorization": `${client.token}`,
+                },
+                "method": "PUT",
+            }, (err, response, body) => {
+            });
+        }, 350);
+    }
+})
+
+client.on('ready', async () => {
+    console.log(`${client.user.tag}`)
+})
+
+client.login("ton token")
+
+//apoo",<#9999
+//commencer pas a dire gngngnn tu token grab chp quoi 
